383dd2751140238c90a15e15ee214e629c838bc8
harivarshan-nitt <harivarshan.nittrichy@gmail.com>
2023-04-27 00:36:10 +0530

    feat : login

--- Backend/src/controllers/auth.js
+++ Backend/src/controllers/auth.js
@@ -1,11 +1,38 @@
 const getAppData = require('../middleware/getAppData');
+const { createJWT } = require('../middleware/jwt');
+const axios = require('axios');
+const Employee = require("../models/schema/employee")
 
 const authRouter = require('express').Router();
 
 authRouter.post("/login", getAppData, async (req, res)=>{
     try{
-        // yet to make api call to get access code and api call to fetch user data and store in db
-        return res.status(200).json({message: "Login Successfull"})
+        let  {clientAppId, clientAppSecret , auth_code} = req.body;
+
+        let resData = await axios.post(process.env.AUTHX_URL+"/oauth/v2/access_token/",{
+            clientAppId:String(clientAppId),
+            clientSecret:String(clientAppSecret),
+            auth_code : auth_code
+
+        })
+        let access_token = resData.data.access_token;
+        let config = {
+            headers:{
+                "Authorization":"token "+String(access_token)
+            }
+        }
+        resData = await axios.get(process.env.AUTHX_URL+"/api/user/",config);
+
+        let user = resData.data.data;
+
+        let foundUser = await Employee.findOne({email : user.EMAIL});
+        if(foundUser != null)
+        {
+            await Employee.create({name:user.NAME,email:user.EMAIL});
+        }
+        let jwt_token = await createJWT(foundUser);
+
+        return res.status(200).json({message: "Login Successfull",jwt_token})
     }
     catch(err){
         console.log(err);
