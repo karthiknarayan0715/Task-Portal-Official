83671225ce149ebcc37d3cb216ea80f6d6482533
harivarshan-nitt <harivarshan.nittrichy@gmail.com>
2023-04-28 01:11:09 +0530

    feat : basic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,33 @@
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './routes/Home';
+import Authorize from './routes/Authorize';
+import Task from './routes/Task';
+import is_admin from "./helpers/is_admin";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [admin, setAdmin] = useState(is_admin);
   return (
     <>
-    
+    <Navbar admin={admin} setAdmin={setAdmin} />
+    <RouterProvider router={createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/authorize",
+    element: <Authorize />
+  },
+  {
+    path: "/home",
+    element: <Task admin={admin}/>
+  },
+])} />
     
     
     </>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,14 +2,21 @@ import { useNavigate } from "react-router-dom"
 import is_logged_in from "../helpers/is_logged_in"
 import { cool_300, cool_50, cool_600, cool_700, cool_900 } from "../themes/main_theme"
 import Text from "./Text"
+import { Button} from '@mantine/core';
+import AdminModal from "./AdminModal";
+import { useEffect, useState } from "react";
 
-const Navbar = (props)=>{
+
+const Navbar = ({admin,setAdmin})=>{
+    const [opened, setOpened] = useState(false);
+    
     const navigate = useNavigate
     return (
-        <div className="navbar" style={{
+        
+        <><div className="navbar" style={{
             height: "10vh",
             display: "flex",
-            alignItems: "center",   
+            alignItems: "center",
             backgroundColor: cool_900
         }}>
             <div className="text" style={{
@@ -17,11 +24,12 @@ const Navbar = (props)=>{
                 fontSize: "32px",
                 marginLeft: "20px"
             }}><Text size="32px" weight="bold">AuthX</Text></div>
+            
             <div className="to_right" style={{
                 marginLeft: "auto",
                 marginRight: "30px"
             }}>
-                {is_logged_in() ? 
+                {is_logged_in() ?
                     <a href="/logout"><div className="nav-button" style={{
                         width: "90px",
                         height: "40px",
@@ -31,7 +39,7 @@ const Navbar = (props)=>{
                         borderRadius: "40px",
                         color: cool_50,
                         cursor: "pointer",
-                        border: "1px solid "+cool_600
+                        border: "1px solid " + cool_600
                     }}>Logout</div></a>
                     :
                     <a href="/login"><div className="nav-button" style={{
@@ -43,11 +51,14 @@ const Navbar = (props)=>{
                         borderRadius: "40px",
                         color: cool_50,
                         cursor: "pointer",
-                        border: "1px solid "+cool_600
-                    }}>Logout</div></a>
-                }
+                        border: "1px solid " + cool_600
+                    }}>Logout</div></a>}
             </div>
+            {!admin &&<Button onClick={()=>setOpened(true)}>Admin</Button>}
+            {admin && <Button onClick={()=> {localStorage.removeItem("admin"); setAdmin(false)}}>Admin Logout</Button>}
         </div>
+        <AdminModal opened={opened} setOpened={setOpened} setAdmin={setAdmin}/>
+        </>
     )
 }
 
